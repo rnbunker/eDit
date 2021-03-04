b3702f2d6674f170b3b6124a836aaa054de82e7d
rnb <rnbwastaken@tutanota.com>
2021-03-04 12:44:38 +0100

    Added the Program to the Repo

---
+++ src/app.d
@@ -0,0 +1,53 @@
+import std.stdio;
+import std.file;
+import core.exception;
+
+int main(string[] args){
+    string rfilename;
+    string wfilename;
+    string filecontent;
+    string rfilecontent;
+    string rowselection;
+    
+    try{
+        if(args[1] != ""){
+            try{
+                writeln("Contents of file \"", args[1], "\":");
+                rfilecontent = readText(args[1]);
+                writeln(rfilecontent);
+                return 0;
+            } catch(FileException){
+                writeln("The file you specified does not exist!");
+                return 1;
+            }
+        }
+    } catch(RangeError){
+        
+    }
+    write("Do you want to read from a file or write to a file? (Type r or w): ");
+    readf("%s\n", rowselection);
+    if(rowselection == "r"){
+        write("Please enter a file to read from: ");
+        readf("%s\n", rfilename);
+        try{ 
+            rfilecontent = readText(rfilename);
+            writeln("Contents of file \"", rfilename, "\":");
+            writeln(rfilecontent);
+            return 0;
+        } catch(FileException){
+            writeln("The file you specified does not exist!");
+            return 1;
+        }
+    } else if(rowselection == "w"){
+        write("Please enter a filename for the new file you want to create: ");
+        readf("%s\n", wfilename);
+        write("Please enter the text you want to write to the file: ");
+        readf("%s\n", filecontent);
+        std.file.write(wfilename, filecontent);
+        writeln("Wrote ", filecontent.length, " Chars to the file \"", wfilename, "\"");
+        return 0;
+    } else {
+        writeln("Please enter a valid Selection and try again");
+        return 1;
+    }
+}
